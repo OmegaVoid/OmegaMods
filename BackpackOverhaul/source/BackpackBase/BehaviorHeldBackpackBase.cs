23e1d2d4de4ef319d8df7ad9d90500e0fb894563
RolfSteiner <lukaseberle2501@gmail.com>
2026-04-30 16:33:20 +0200

    Added BackpackBase

---
+++ BackpackOverhaul/source/BackpackBase/BehaviorHeldBackpackBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
+
+namespace BackpackOverhaul.BackpackSystem
+{
+    public class CollectibleBehaviorHeldBackpackBase : CollectibleBehaviorHeldBag, IHeldBag, IAttachedListener
+    {
+        public CollectibleBehaviorHeldBackpackBase(CollectibleObject collObj) : base(collObj)
+        {
+        }
+        public override TagSet GetStorageTags(ItemStack bagStack)
+        {
+            return bagStack.ItemAttributes["backpack"]["storageTags"].AsObject<TagSet>();
+        }
+    }
+}
