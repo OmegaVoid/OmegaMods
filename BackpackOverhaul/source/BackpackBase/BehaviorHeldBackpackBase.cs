d0a90e0c95d7bb149fe55408de5b037eae101eff
RolfSteiner <lukaseberle2501@gmail.com>
2026-05-01 23:59:40 +0200

    🧹 Clean up CollectibleBehaviorHeldBackpackBase
    Removed unused constructor variable and redundant method overrides for clarity and maintainability.

--- BackpackOverhaul/source/BackpackBase/BehaviorHeldBackpackBase.cs
+++ BackpackOverhaul/source/BackpackBase/BehaviorHeldBackpackBase.cs
@@ -14,23 +14,10 @@ namespace BackpackOverhaul.BackpackSystem
 {
     public class CollectibleBehaviorHeldBackpackBase : CollectibleBehaviorHeldBag, IHeldBag, IAttachedListener
     {
-        public CollectibleBehaviorHeldBackpackBase(CollectibleObject collObj) : base(collObj)
-        {
-            EntityBehaviorPlayerInventory? Test;
-        }
+        public CollectibleBehaviorHeldBackpackBase(CollectibleObject collObj) : base(collObj) { }
         public override TagSet GetStorageTags(ItemStack bagStack)
         {
             return bagStack.ItemAttributes["backpack"]["storageTags"].AsObject<TagSet>();
-        }
-        public override void GetHeldItemInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo)
-        {
-            base.GetHeldItemInfo(inSlot, dsc, world, withDebugInfo);
-        }
-        public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handHandling, ref EnumHandling handling)
-        {
-            EntityPlayer entityPlayer = byEntity as EntityPlayer;
-            EntityBehaviorPlayerInventory inv = entityPlayer.GetBehavior<EntityBehaviorPlayerInventory>();
-            base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handHandling, ref handling);
-        }        
+        }  
     }
 }
