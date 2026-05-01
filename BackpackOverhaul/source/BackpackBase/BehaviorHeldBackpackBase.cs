2603795de380702a7b50c12bd82634b018a99357
RolfSteiner <lukaseberle2501@gmail.com>
2026-05-01 23:36:38 +0200

    🔧 Update backpack system: deps, shapes, and rendering
    Upgraded InsanityLib and OmegaVoid SDK dependencies, improved launch settings, added new backpack shapes and attached entity JSONs, enhanced item definitions, extended behavior logic, and implemented dynamic wearable shape rendering for composite backpacks.

--- BackpackOverhaul/source/BackpackBase/BehaviorHeldBackpackBase.cs
+++ BackpackOverhaul/source/BackpackBase/BehaviorHeldBackpackBase.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using InsanityLib;
 using InsanityLib.Generators.Attributes;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 
 namespace BackpackOverhaul.BackpackSystem
@@ -13,10 +16,21 @@ namespace BackpackOverhaul.BackpackSystem
     {
         public CollectibleBehaviorHeldBackpackBase(CollectibleObject collObj) : base(collObj)
         {
+            EntityBehaviorPlayerInventory? Test;
         }
         public override TagSet GetStorageTags(ItemStack bagStack)
         {
             return bagStack.ItemAttributes["backpack"]["storageTags"].AsObject<TagSet>();
         }
+        public override void GetHeldItemInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo)
+        {
+            base.GetHeldItemInfo(inSlot, dsc, world, withDebugInfo);
+        }
+        public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handHandling, ref EnumHandling handling)
+        {
+            EntityPlayer entityPlayer = byEntity as EntityPlayer;
+            EntityBehaviorPlayerInventory inv = entityPlayer.GetBehavior<EntityBehaviorPlayerInventory>();
+            base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handHandling, ref handling);
+        }        
     }
 }
---
+++ BackpackOverhaul/source/BackpackBase/ItemBackpackBase.cs
@@ -0,0 +1,43 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.Common;
+using Vintagestory.GameContent;
+
+namespace BackpackOverhaul.BackpackBase
+{
+    public class ItemBackpackBase: Item, IWearableShapeSupplier
+    {
+        private Shape? shape;
+        private string? shapePath;
+        public override void OnLoaded(ICoreAPI api)
+        {
+            shapePath = IAttachableToEntity.FromAttributes(this)?.GetAttachedShape(new ItemStack(), "backpack")?.Base.CopyWithPathPrefixAndAppendixOnce("shapes/", ".json");
+            base.OnLoaded(api);
+        }
+        public Shape? GetShape(ItemStack itemStack, Entity targetEntity, string texturePrefixCode)
+        {
+            string[] p = ["W","E","top"];
+            int i = 0;
+            IDictionary<string, CompositeTexture> collectedTextures = this.Textures;
+            ITextureAtlasAPI? targetAtlas = (api as ICoreClientAPI)?.ItemTextureAtlas;
+            shape = Vintagestory.API.Common.Shape.TryGet(api, shapePath);
+            ItemSlot[]? t = (api.World.AllPlayers.First((item) => item.Entity == targetEntity).InventoryManager.Inventories.First((item) => item.Value.ClassName == "backpack").Value as InventoryPlayerBackpacks)?.bagSlots.Where((item) => item.Itemstack != null).Where((item) => item.Itemstack!.Item != this).ToArray();
+            string? childPath = null;
+            if (shape != null & targetAtlas != null & shapePath != null & t != null)
+            {
+                foreach (ItemSlot slot in t!)
+                {
+                    childPath = slot.Itemstack!.Item.Attributes["backpack"]["attachedShape"].AsObject<CompositeShape>(null, slot.Itemstack.Item.Code.Domain)?.Base.CopyWithPathPrefixAndAppendixOnce("shapes/", ".json");
+                    if (Vintagestory.API.Common.Shape.TryGet(api,childPath) is Shape childShape)
+                    {
+                        childShape.Elements[0].StepParentName = p[i];
+                        shape!.StepParentShape(childShape, texturePrefixCode, childPath, shapePath, api.World.Logger, (texcode, tloc) => EntityBehaviorContainer.addTexture((api as ICoreClientAPI), texcode, tloc, collectedTextures, texturePrefixCode, targetAtlas));
+                        i += 1;
+                    }
+                }
+            }
+            return shape;
+        }
+    }
+}
