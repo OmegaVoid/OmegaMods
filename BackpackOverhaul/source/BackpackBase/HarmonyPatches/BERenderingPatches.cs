aca31765b70c7072cd466b410856e23ea30326a5
RolfSteiner <lukaseberle2501@gmail.com>
2026-05-03 19:44:48 +0200

    :sparkles: Add mesh caching and refactor backpack rendering
    Replaced BERenderingPatch with BERenderingPatches to add mesh caching for backpack items and cleanup on block removal. Refactored ItemBackpackBase to use ShapeTextureSource, improved shape/texture handling, and updated NuGet package and debug settings.

---
+++ BackpackOverhaul/source/BackpackBase/HarmonyPatches/BERenderingPatches.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using System.Diagnostics;
+using System.Security.AccessControl;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+
+namespace BackpackOverhaul.BackpackBase.HarmonyPatches
+{
+
+    [HarmonyPatch(typeof(BlockEntityGroundStorage), "OnTesselation")]
+    public static class BERenderingPatche
+    {
+        public static bool Prefix(BlockEntityGroundStorage __instance, ITerrainMeshPool mesher, ITesselatorAPI tesselator)
+        {
+            if (__instance.Inventory.FirstNonEmptySlot?.Itemstack?.Item is ItemBackpackBase backpack)
+             {
+                bool tryed = (__instance.Api as ICoreClientAPI)!.ObjectCache.TryGetValue("groundstorage-mesh-backpackbase-" + __instance.Pos.ToString(), out var _mesh);
+                var mesh = _mesh as MeshData;
+                if (!tryed)
+                {
+                    tesselator.TesselateShape("mylog",backpack._shape, out mesh, backpack.texSource);
+                    (__instance.Api as ICoreClientAPI)!.ObjectCache.Add("groundstorage-mesh-backpackbase-" + __instance.Pos.ToString(), mesh);
+                }
+                mesher.AddMeshData(mesh);
+            }
+            return false;
+        }
+    }
+    [HarmonyPatch(typeof(BlockEntityGroundStorage), "OnBlockRemoved")]
+    public static class BERenderingPatch2
+    {
+        public static bool Prefix(BlockEntityGroundStorage __instance)
+        {
+            if (__instance.Inventory.FirstNonEmptySlot?.Itemstack?.Item is ItemBackpackBase backpack)
+            {
+                (__instance.Api as ICoreAPI)!.ObjectCache.Remove("groundstorage-mesh-backpackbase-" + __instance.Pos.ToString());
+            }
+            return false;
+        }
+    }
+}
--- BackpackOverhaul/source/BackpackBase/ItemBackpackBase.cs
+++ BackpackOverhaul/source/BackpackBase/ItemBackpackBase.cs
@@ -3,17 +3,18 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.Common;
 using Vintagestory.GameContent;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BackpackOverhaul.BackpackBase
 {
     public class ItemBackpackBase: Item, IWearableShapeSupplier
     {
         public Shape? _shape;
-        public Shape? shape { get; private set; }
+        private Shape? shape;
         private string? shapePath;
         private string? baseShapePath;
         private Shape? baseShape;
-        public int[]? texids;
+        public ShapeTextureSource? texSource;
         public override void OnLoaded(ICoreAPI api)
         {
             baseShapePath = Shape.Base.CopyWithPathPrefixAndAppendixOnce("shapes/", ".json");
@@ -30,17 +31,18 @@ namespace BackpackOverhaul.BackpackBase
             ITextureAtlasAPI? basetargetAtlas = (api as ICoreClientAPI)?.BlockTextureAtlas;
             shape = Vintagestory.API.Common.Shape.TryGet(api, shapePath);
             ItemSlot[]? t = (api.World.AllPlayers.First((item) => item.Entity == targetEntity).InventoryManager.Inventories.First((item) => item.Value.ClassName == "backpack").Value as InventoryPlayerBackpacks)?.bagSlots.Where((item) => item.Itemstack != null).Where((item) => item.Itemstack!.Item != this).ToArray();
-            string? childPath = null;
+            string childPath = "";
             if (shape != null & targetAtlas != null & shapePath != null & t != null)
+                _shape = baseShape!.Clone();
                 foreach (ItemSlot slot in t!)
-                {
-                    childPath = slot.Itemstack!.Item.Attributes["backpack"]["attachedShape"].AsObject<CompositeShape>(null, slot.Itemstack.Item.Code.Domain)?.Base.CopyWithPathPrefixAndAppendixOnce("shapes/", ".json");
+                    {
+                    childPath = slot.Itemstack!.Item.Attributes["backpack"]["attachedShape"].AsObject<CompositeShape>(null, slot.Itemstack.Item.Code.Domain)?.Base.CopyWithPathPrefixAndAppendixOnce("shapes/", ".json") ?? "";
                     if (Vintagestory.API.Common.Shape.TryGet(api,childPath) is Shape childShape)
                     {
                         childShape.Elements[0].StepParentName = p[i];                    
                         shape!.StepParentShape(childShape, texturePrefixCode, childPath, shapePath, api.World.Logger, (texcode, tloc) => EntityBehaviorContainer.addTexture((api as ICoreClientAPI), texcode, tloc, collectedTextures, texturePrefixCode, targetAtlas));
-                        baseShape!.StepParentShape(childShape, texturePrefixCode, childPath, shapePath, api.World.Logger, (texcode, tloc) => EntityBehaviorContainer.addTexture((api as ICoreClientAPI), texcode, tloc, collectedTextures, texturePrefixCode, targetAtlas));
-                        _shape ??= baseShape;
+                        _shape!.StepParentShape(childShape, texturePrefixCode, childPath, shapePath, api.World.Logger, (texcode, tloc) => EntityBehaviorContainer.addTexture((api as ICoreClientAPI), texcode, tloc, collectedTextures, texturePrefixCode, targetAtlas));
+                        texSource = new ShapeTextureSource((api as ICoreClientAPI)!, _shape, "mylog", collectedTextures, (p) => p);
                         i += 1;
                     }
                 }
